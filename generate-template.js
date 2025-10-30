e9525dd0ba3de87fd8398ed82c840ce7c76b3bd2
planetabhi <1704622+planetabhi@users.noreply.github.com>
2025-10-31 03:52:42 +0530

    feat: polishing

--- generate-template.js
+++ generate-template.js
@@ -214,14 +214,14 @@ const fullHtmlContent = `<!DOCTYPE html>
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.41 13.28C7.332 10.205 6.716 5.693 8.357 2c-1.23.41-2.256 1.23-3.281 2.256a10.4 10.4 0 0 0 0 14.768c4.102 4.102 10.46 3.897 14.562-.205 1.026-1.026 1.846-2.051 2.256-3.282-3.896 1.436-8.409.82-11.486-2.256"/>
         </svg>
         <svg id="icon-sun" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" aria-hidden="true" style="display:none">
-          <g clip-path="url(#a)">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M3 12H1m22 0h-2m-9 9v2m0-22v2M5.636 18.364l-1.414 1.414M19.778 4.222l-1.414 1.414m-12.728 0L4.222 4.222m15.556 15.556-1.414-1.414M18 12a6 6 0 1 1-12 0 6 6 0 0 1 12 0"/>
-          </g>
-          <defs>
-          <clipPath id="a">
-            <path fill="#fff" d="M0 0h24v24H0z"/>
-          </clipPath>
-          </defs>
+        <g clip-path="url(#a)">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M5 12H1m22 0h-4M7.05 7.05 4.222 4.222m15.556 15.556L16.95 16.95m-9.9 0-2.828 2.828M19.778 4.222 16.95 7.05M12 19v4m0-22v4m4 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0"/>
+        </g>
+        <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h24v24H0z"/>
+        </clipPath>
+        </defs>
         </svg>
         </button>
       </div>
@@ -458,9 +458,10 @@ const fullHtmlContent = `<!DOCTYPE html>
       var zoomOutBtn = document.getElementById('zoom-out');
       
       function applyZoom(zoomLevel) {
-        if (iconGrid) {
-          iconGrid.style.zoom = zoomLevel;
-        }
+        if (!iconGrid) return;
+        iconGrid.style.transform = '';
+        iconGrid.style.zoom = '';
+        iconGrid.style.setProperty('--zoom', String(zoomLevel));
       }
       
       function updateButtons() {
